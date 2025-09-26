b291409523021b6589e5b785c6f08d3a179ad31a
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-09-26 14:33:16 +0530

    feat: start server with initials

--- backend/SETUP.md
+++
@@ -1,1 +0,0 @@
-npm i express cors mongoose dotenv nodemon jsonwebtoken bcryptjs nodemailer cookie-parser
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json("API is working");
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port: ${port}`);
+});
