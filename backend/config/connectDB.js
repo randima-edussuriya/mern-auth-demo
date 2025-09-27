0cf3dca8c7918c81a179721ccf7ad3db37eaf3e1
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-09-27 09:39:41 +0530

    feat: config database connection

---
+++ backend/config/connectDB.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth-demo`);
+    console.log("Initial database connection successfull");
+  } catch (error) {
+    console.log("Initial database connection failed:", error);
+  }
+
+  //setup event listeners
+  mongoose.connection.on("error", (error) => {
+    console.log("Database error:", error);
+  });
+  mongoose.connection.on("disconnected", () => {
+    console.log("Database disconnected");
+  });
+};
+
+export default connectDB;
