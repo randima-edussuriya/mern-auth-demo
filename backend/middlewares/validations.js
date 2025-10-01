50c27ef5a7d5bbe01695e495b49c7a41a9f9231b
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-10-01 16:32:12 +0530

    feat: create a middleware for validation

---
+++ backend/middlewares/validations.js
@@ -0,0 +1,33 @@
+import validator from "validator";
+
+export const validateRegister = (req, res, next) => {
+  const name = req.body.name?.trim();
+  const email = req.body.email?.trim();
+  const password = req.body.password?.trim();
+
+  // chechk empty
+  if (!name || !email || !password)
+    return res
+      .status(400)
+      .json({ success: false, message: "Fields are required" });
+  //validte email
+  if (!validator.isEmail(email))
+    return res
+      .status(400)
+      .json({ success: false, message: "Invalid email format" });
+
+  //validate password complexity
+  if (
+    !validator.isStrongPassword(password, {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    })
+  )
+    return res.status(400).json({ success: false, message: "Weak password" });
+
+  req.body = { name, email, password };
+  next();
+};
