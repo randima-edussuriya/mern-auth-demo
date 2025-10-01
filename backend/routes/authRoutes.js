8c3f860dc95fd33340c0b77bb4558e90391a3f6c
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-10-01 20:20:00 +0530

    feat: add router for login API

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { register } from "../controllers/authController.js";
-import { validateRegister } from "../middlewares/validations.js";
+import { login, register } from "../controllers/authController.js";
+import { validateLogin, validateRegister } from "../middlewares/validations.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", validateRegister, register);
+authRouter.post("/login", validateLogin, login);
 
 export default authRouter;
