8bfe10c215b148a33dde76df8154d0f6c23f2823
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-10-04 19:12:36 +0530

    feat: add API endpoint for logout

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { login, register } from "../controllers/authController.js";
+import { login, logout, register } from "../controllers/authController.js";
 import { validateLogin, validateRegister } from "../middlewares/validations.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", validateRegister, register);
 authRouter.post("/login", validateLogin, login);
+authRouter.post("/logout", logout);
 
 export default authRouter;
