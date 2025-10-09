acb0d9c9f12c63ab90e22bf649c6de2271248d42
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-10-09 11:52:50 +0530

    feat: create route for sendVerifyOtp controller

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,22 @@
 import express from "express";
-import { login, logout, register } from "../controllers/authController.js";
-import { validateLogin, validateRegister } from "../middlewares/validations.js";
+import {
+  login,
+  logout,
+  register,
+  sendVerifyOtp,
+} from "../controllers/authController.js";
+import {
+  validateLogin,
+  validateRegister,
+  validateUserId,
+} from "../middlewares/validations.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", validateRegister, register);
 authRouter.post("/login", validateLogin, login);
 authRouter.post("/logout", logout);
+
+authRouter.post("/send-verify-otp", validateUserId, sendVerifyOtp);
 
 export default authRouter;
