aa9caed7f5874d48980e0c12931c213815b647be
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-10-09 15:00:26 +0530

    feat: create route for verifyEmail controller

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,11 +4,13 @@ import {
   logout,
   register,
   sendVerifyOtp,
+  verifyEmail,
 } from "../controllers/authController.js";
 import {
   validateLogin,
   validateRegister,
   validateUserId,
+  validateVerifyEmail,
 } from "../middlewares/validations.js";
 
 const authRouter = express.Router();
@@ -18,5 +20,6 @@ authRouter.post("/login", validateLogin, login);
 authRouter.post("/logout", logout);
 
 authRouter.post("/send-verify-otp", validateUserId, sendVerifyOtp);
+authRouter.post("/verify-email", validateVerifyEmail, verifyEmail);
 
 export default authRouter;
