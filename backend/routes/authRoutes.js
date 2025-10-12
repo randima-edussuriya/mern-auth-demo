bd66b737b81e7721f87852c988a6362e7515461c
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-10-12 12:52:32 +0530

    docs: add comments

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -18,13 +18,19 @@ import userAuth from "../middlewares/userAuth.js";
 
 const authRouter = express.Router();
 
+//register, login, logout
 authRouter.post("/register", validateRegister, register);
 authRouter.post("/login", validateLogin, login);
 authRouter.post("/logout", logout);
 
+//email vaerification
 authRouter.post("/send-verify-otp", userAuth, sendVerifyOtp);
 authRouter.post("/verify-email", userAuth, validateOtp, verifyEmail);
+
+//check authentication
 authRouter.post("/is-authenticated", userAuth, isAuthenticated);
+
+//reset password
 authRouter.post("/send-reset-otp", validateEmail, sendResetOtp);
 
 export default authRouter;
