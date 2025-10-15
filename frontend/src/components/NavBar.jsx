de1c7df7aa9c78da4ae5bece1f66368a6ed8e3e1
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-10-15 20:35:19 +0530

    style: add cursor poiter for logo, change login button color

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,8 +4,8 @@ import arrowIcon from "../assets/arrow_icon.svg";
 function NavBar() {
   return (
     <div className="absolute top-0 flex items-center justify-between w-full p-4 sm:p-6">
-      <img src={logo} alt="Logo" className="w-28 sm:w-32" />
-      <button className="flex items-center gap-2 px-6 py-2 transition-all border border-gray-500 rounded-full cursor-pointer hover:bg-gray-200">
+      <img src={logo} alt="Logo" className="cursor-pointer w-28 sm:w-32" />
+      <button className="flex items-center gap-2 px-6 py-2 transition-all border border-blue-600 rounded-full cursor-pointer hover:bg-blue-200">
         Login
         <img src={arrowIcon} alt="arrowIcon" />
       </button>
