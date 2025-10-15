c833667d1199d9538a8ecaf6ab108cad1676e2bb
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-10-15 11:10:57 +0530

    style: keep login text as black

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
   return (
     <div className="absolute top-0 flex items-center justify-between w-full p-4 sm:p-6">
       <img src={logo} alt="Logo" className="w-28 sm:w-32" />
-      <button className="flex items-center gap-2 px-6 py-2 text-gray-600 transition-all border border-gray-500 rounded-full cursor-pointer hover:bg-gray-200">
+      <button className="flex items-center gap-2 px-6 py-2 transition-all border border-gray-500 rounded-full cursor-pointer hover:bg-gray-200">
         Login
         <img src={arrowIcon} alt="arrowIcon" />
       </button>
