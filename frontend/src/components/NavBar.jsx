b6762620b298cef43b7083150c0456d1731d44cb
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-10-16 06:43:04 +0530

    feat: add login btn navigation

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,11 +1,16 @@
 import logo from "../assets/logo.svg";
 import arrowIcon from "../assets/arrow_icon.svg";
+import { useNavigate } from "react-router-dom";
 
 function NavBar() {
+  const navigate = useNavigate();
   return (
     <div className="absolute top-0 flex items-center justify-between w-full p-4 sm:p-6">
       <img src={logo} alt="Logo" className="cursor-pointer w-28 sm:w-32" />
-      <button className="flex items-center gap-2 px-6 py-2 transition-all border border-blue-600 rounded-full cursor-pointer hover:bg-blue-200">
+      <button
+        onClick={() => navigate("/login")}
+        className="flex items-center gap-2 px-6 py-2 transition-all border border-blue-600 rounded-full cursor-pointer hover:bg-blue-200"
+      >
         Login
         <img src={arrowIcon} alt="arrowIcon" />
       </button>
