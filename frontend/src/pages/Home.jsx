93c8cfc7ce5b6a88de083834f12dfd19e564b774
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-10-17 00:48:44 +0530

    fix: fix overflow hidden

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 
 function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-[url('bg_image.png')] bg-cover bg-center">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[url('bg_image.png')] bg-cover bg-center">
       <NavBar />
       <Header />
     </div>
