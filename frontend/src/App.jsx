72304e7e9d8ef42da9cc092a3445f48346463276
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-10-16 12:32:41 +0530

    refactor: rename Login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import EmailVerify from "./pages/EmailVerify";
-import Login from "./pages/Login";
+import LoginRegister from "./pages/LoginRegister";
 import ResetPassword from "./pages/ResetPassword";
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<LoginRegister />} />
           <Route path="/email-verify" element={<EmailVerify />} />
           <Route path="/reset-password" element={<ResetPassword />} />
         </Routes>
