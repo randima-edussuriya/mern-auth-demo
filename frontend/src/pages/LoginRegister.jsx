7966c9007d192531e399b1a886bce19dbca02932
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-10-17 07:09:42 +0530

    feat: separate login, sign up using useState and add error message UI

--- frontend/src/pages/LoginRegister.jsx
+++ frontend/src/pages/LoginRegister.jsx
@@ -1,66 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import lock_icon from "../assets/lock_icon.svg";
 import mail_icon from "../assets/mail_icon.svg";
 import person_icon from "../assets/person_icon.svg";
 
 function LoginRegister() {
+  const [isLogin, setIsLogin] = useState(false);
   return (
     <div className="flex items-center justify-center min-h-screen  bg-gradient-to-br from-blue-300 to-green-300 px-6 sm:px-0">
       <div className="w-full sm:w-100 p-10 text-indigo-300 rounded-lg shadow-lg bg-slate-900">
         <img
-          className="w-12 mx-auto mb-3 sm:w-15"
+          className="mx-auto mb-3 w-13 sm:w-15"
           src="favicon.svg"
           alt="Logo"
         />
         <h1 className="mb-3 text-3xl font-semibold text-center text-white">
-          Login
+          {isLogin ? "Login" : "Sign Up"}
         </h1>
-        <p className="mb-4 text-center">Create your account</p>
+        <p className="mb-4 text-center">
+          {isLogin ? "Log in to your account" : "Create your account"}
+        </p>
         <form>
-          <div className="flex bg-slate-700 rounded-full gap-3 mb-4 px-5 py-2.5 w-full">
-            <img src={person_icon} alt="person icon" />
-            <input
-              className="w-full bg-transparent outline-none"
-              type="text"
-              placeholder="Full Name"
-              required
-            />
+          {!isLogin && (
+            <div className="mb-4">
+              <div className="flex bg-slate-700 rounded-full gap-3 px-5 py-2.5 w-full">
+                <img className="w-4" src={person_icon} alt="person icon" />
+                <input
+                  className="w-full bg-transparent outline-none"
+                  type="text"
+                  placeholder="Full Name"
+                  required
+                />
+              </div>
+              <span className="text-red-400">error message for field</span>
+            </div>
+          )}
+          <div className="mb-4">
+            <div className="flex bg-slate-700 rounded-full gap-3 px-5 py-2.5 w-full">
+              <img className="w-4" src={mail_icon} alt="mail icon" />
+              <input
+                className="w-full bg-transparent outline-none"
+                type="email"
+                placeholder="Email Address"
+                required
+              />
+            </div>
+            <span className="text-red-400">error message for field</span>
           </div>
-          <div className="flex bg-slate-700 rounded-full gap-3 mb-4 px-5 py-2.5 w-full">
-            <img src={mail_icon} alt="mail icon" />
-            <input
-              className="w-full bg-transparent outline-none"
-              type="email"
-              placeholder="Email Address"
-              required
-            />
+          <div className="mb-4">
+            <div className="flex bg-slate-700 rounded-full gap-3 px-5 py-2.5 w-full">
+              <img className="w-4" src={lock_icon} alt="person icon" />
+              <input
+                className="w-full bg-transparent outline-none"
+                type="password"
+                placeholder="Password"
+                required
+              />
+            </div>
+            <span className="text-red-400">error message for field</span>
           </div>
-          <div className="flex bg-slate-700 rounded-full gap-3 mb-4 px-5 py-2.5 w-full">
-            <img src={lock_icon} alt="person icon" />
-            <input
-              className="w-full bg-transparent outline-none"
-              type="password"
-              placeholder="Password"
-              required
-            />
-          </div>
-          <p className="cursor-pointer mb-4">Forgot Password?</p>
+
+          {isLogin && <p className="cursor-pointer mb-4">Forgot Password?</p>}
           <button className="mb-4 rounded-full w-full py-2 bg-gradient-to-br from-blue-900 to-blue-400 cursor-pointer text-white font-medium">
-            Sign Up
+            {isLogin ? "Login" : "Sign Up"}
           </button>
         </form>
-        <p className="text-center mt-4 text-gray-400">
-          Already have an account?{" "}
-          <span className="underline cursor-pointer text-blue-400">
-            Login here
-          </span>
-        </p>
-        <p className="text-center mt-4 text-gray-400">
-          Don't have an account?{" "}
-          <span className="underline cursor-pointer text-blue-400">
-            Sign Up
-          </span>
-        </p>
+        {isLogin ? (
+          <p className="text-center mt-4 text-gray-400">
+            Don't have an account?{" "}
+            <span
+              onClick={() => setIsLogin(false)}
+              className="underline cursor-pointer text-blue-400"
+            >
+              Sign Up
+            </span>
+          </p>
+        ) : (
+          <p className="text-center mt-4 text-gray-400">
+            Already have an account?{" "}
+            <span
+              onClick={() => setIsLogin(true)}
+              className="underline cursor-pointer text-blue-400"
+            >
+              Login here
+            </span>
+          </p>
+        )}
       </div>
     </div>
   );
