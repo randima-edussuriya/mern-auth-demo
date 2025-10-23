d0e8b5ad947a587510a444b993250a18634e06f0
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-10-23 11:49:58 +0530

    refactor: keep ProfileMenu as sperate component for clean code

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,15 +3,13 @@ import arrowIcon from "../assets/arrow_icon.svg";
 import { useNavigate } from "react-router-dom";
 import { useContext, useEffect, useRef, useState } from "react";
 import { AppContext } from "../context/AppContext";
-import axios from "axios";
-import { toast } from "react-toastify";
+import ProfileMenu from "./ProfileMenu";
 
 function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const menuRef = useRef(null);
 
-  const { userData, backendUrl, setIsLoggedIn, setUserData } =
-    useContext(AppContext);
+  const { userData } = useContext(AppContext);
 
   const navigate = useNavigate();
 
@@ -29,73 +27,17 @@ function NavBar() {
     };
   }, []);
 
-  //user logout
-  const handleLogout = async () => {
-    try {
-      const { data } = await axios.post(`${backendUrl}/api/auth/logout`);
-      if (data.success) {
-        setIsLoggedIn(false);
-        setUserData(false);
-        navigate("/");
-      }
-    } catch (error) {
-      toast.error("Something went wrong, Please try again later");
-      console.error(error);
-    }
-  };
-
-  //send verification OTP
-  const sendVerificationOtp = async () => {
-    try {
-      const { data } = await axios.post(
-        `${backendUrl}/api/auth/send-verify-otp`
-      );
-      if (data.success) {
-        toast.success(data.message);
-        navigate("/email-verify");
-      }
-    } catch (error) {
-      if (error.response?.data?.message) {
-        toast.error(error.response.data.message);
-      } else {
-        toast.error("Something went wrong, Please try again later");
-      }
-      console.error(error);
-    }
-  };
-
   return (
     <div className="absolute top-0 flex items-center justify-between w-full p-4 sm:p-6">
       <img src={logo} alt="Logo" className="w-28 sm:w-32" />
       {userData ? (
         <div
+          ref={menuRef}
           onClick={() => setIsMenuOpen(!isMenuOpen)}
-          className="bg-blue-600 text-white rounded-full flex justify-center items-center w-8 h-8 sm:w-9 sm:h-9 relative group"
+          className="relative bg-blue-600 text-white rounded-full flex justify-center items-center w-8 h-8 sm:w-9 sm:h-9 cursor-pointer"
         >
           {userData.name[0].toUpperCase()}
-          <div
-            ref={menuRef}
-            className={`absolute group-hover:block ${
-              isMenuOpen ? "block" : "hidden"
-            } transition-all text-black top-0 right-0 z-10 pt-10`}
-          >
-            <ul className="list-none p-2 bg-gray-100 rounded">
-              {userData.isAccountVerified === false && (
-                <li
-                  onClick={sendVerificationOtp}
-                  className="py-1 px-2 hover:bg-gray-200 cursor-pointer rounded"
-                >
-                  Verify Email
-                </li>
-              )}
-              <li
-                onClick={handleLogout}
-                className="py-1 px-2 hover:bg-gray-200 cursor-pointer rounded pr-11"
-              >
-                Logout
-              </li>
-            </ul>
-          </div>
+          {isMenuOpen && <ProfileMenu setIsMenuOpen={setIsMenuOpen} />}
         </div>
       ) : (
         <button
---
+++ frontend/src/components/ProfileMenu.jsx
@@ -0,0 +1,72 @@
+import axios from "axios";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { AppContext } from "../context/AppContext";
+
+function ProfileMenu({ setIsMenuOpen }) {
+  const { userData, backendUrl, setIsLoggedIn, setUserData } =
+    useContext(AppContext);
+
+  const navigate = useNavigate();
+
+  //user logout
+  const handleLogout = async () => {
+    try {
+      const { data } = await axios.post(`${backendUrl}/api/auth/logout`);
+      if (data.success) {
+        setIsLoggedIn(false);
+        setUserData(false);
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error("Something went wrong, Please try again later");
+      console.error(error);
+    } finally {
+      setIsMenuOpen(false);
+    }
+  };
+
+  //send verification OTP
+  const sendVerificationOtp = async () => {
+    try {
+      const { data } = await axios.post(
+        `${backendUrl}/api/auth/send-verifyotp`
+      );
+      if (data.success) {
+        toast.success(data.message);
+        navigate("/email-verify");
+      }
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message ||
+          "Something went wrong, Please try again later"
+      );
+      console.error(error);
+    } finally {
+      setIsMenuOpen(false);
+    }
+  };
+  return (
+    <div className="absolute transition-all text-black top-0 right-0 z-10 pt-10">
+      <ul className="list-none p-2 bg-gray-100 rounded">
+        {userData.isAccountVerified === false && (
+          <li
+            onClick={sendVerificationOtp}
+            className="py-1 px-2 hover:bg-gray-200 cursor-pointer rounded"
+          >
+            Verify Email
+          </li>
+        )}
+        <li
+          onClick={handleLogout}
+          className="py-1 px-2 hover:bg-gray-200 cursor-pointer rounded pr-11"
+        >
+          Logout
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default ProfileMenu;
