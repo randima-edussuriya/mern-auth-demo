f486b0f0ec9c86d48819f9e9cbd703143e00f8c1
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-11-01 13:28:43 +0530

    refactor: remove testing console log

--- frontend/src/utils/maskEmail.js
+++ frontend/src/utils/maskEmail.js
@@ -1,7 +1,6 @@
 export const maskEmail = (email) => {
   if (!email) return "";
   const [localPart, doamin] = email.split("@");
-  console.log(email.split("@"));
   if (localPart.length <= 2) return localPart[0] + "***" + "@" + doamin;
   return localPart[0] + "***" + localPart.slice(-1) + "@" + doamin;
 };
