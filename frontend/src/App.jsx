592ccadfdcff8109278fec65af6ce435ad56ac12
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-11-06 09:54:04 +0530

    feat: protect /email-verify for logged users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,26 @@ import EmailVerify from "./pages/EmailVerify";
 import LoginRegister from "./pages/LoginRegister";
 import ResetPassword from "./pages/ResetPassword";
 import Test from "./pages/Test";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import { AppContext } from "./context/AppContext";
+import { useContext } from "react";
 
 function App() {
+  const { isLoggedIn, authChecked } = useContext(AppContext);
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<LoginRegister />} />
-          <Route path="/email-verify" element={<EmailVerify />} />
+          <Route
+            path="/email-verify"
+            element={
+              <ProtectedRoute isLoggedIn={isLoggedIn} authChecked={authChecked}>
+                <EmailVerify />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/test" element={<Test />} />
         </Routes>
