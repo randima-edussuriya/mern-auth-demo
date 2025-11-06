7a3770aa9b20e287d8e8db5b2f7a0ca6b5a964e4
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-11-06 17:52:11 +0530

    feat: add resuable loader

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Loader({ background = "" }) {
+  return (
+    <div
+      role="status"
+      aria-live="polite"
+      aria-busy="true"
+      className={`flex items-center justify-center h-screen ${background}`}
+    >
+      <div className="animate-spin rounded-full h-14 w-14 border-4 border-gray-300 border-t-blue-600"></div>
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+}
+
+export default Loader;
