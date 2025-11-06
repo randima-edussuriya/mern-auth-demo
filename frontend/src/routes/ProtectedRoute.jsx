ee67b42f5f3e41f72795a4e20a41667d91484b0c
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-11-06 17:52:56 +0530

    feat: apply loader while authchecking

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,10 +1,12 @@
 import { Navigate, useLocation } from "react-router-dom";
+import Loader from "../components/Loader";
 
 const ProtectedRoute = ({ isLoggedIn, authChecked, children }) => {
   const location = useLocation();
-  if (!authChecked) return null;
+  if (!authChecked)
+    return <Loader background="bg-linear-to-br from-blue-300 to-green-300" />;
   if (!isLoggedIn) {
-    return <Navigate to="/login"  state={{ from: location }} />;
+    return <Navigate to="/login" state={{ from: location }} />;
   }
 
   return children;
