627af6d51660d854649090ed4f9a1cdf285f044e
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-11-07 13:13:03 +0530

    feat: keep common backgorund

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Loader({ background = "" }) {
+function Loader() {
   return (
     <div
       role="status"
       aria-live="polite"
       aria-busy="true"
-      className={`flex items-center justify-center h-screen ${background}`}
+      className="flex items-center justify-center h-screen"
     >
       <div className="animate-spin rounded-full h-14 w-14 border-4 border-gray-300 border-t-blue-600"></div>
       <span className="sr-only">Loading...</span>
